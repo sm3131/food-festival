9faa71ad07aeea015871b3caee03fadb3e2c1e35
sm3131 <sm2683@nau.edu>
2022-03-07 16:51:49 -0700

    trying to add service worker but not working

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
         tickets: "./assets/js/tickets.js"
     },
     output: {
-        filename: "[name].bundle.js",
         path: __dirname + "/dist",
+        filename: "[name].bundle.js"
     },
     module: {
         rules: [
