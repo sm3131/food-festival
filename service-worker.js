622dfa029a34f220baf2e018640885bb95642830
sm3131 <sm2683@nau.edu>
2022-03-07 21:15:59 -0700

    adding webpack changes

--- service-worker.js
+++ service-worker.js
@@ -1,64 +1,66 @@
-const APP_PREFIX = 'FoodFest-';
-const VERSION = 'version_02';
-const CACHE_NAME = APP_PREFIX + VERSION;
+console.log('service worker connected!');
 
-const FILES_TO_CACHE = [
-    "./index.html",
-    "./events.html",
-    "./tickets.html",
-    "./schedule.html",
-    "./assets/css/style.css",
-    "./assets/css/bootstrap.css",
-    "./assets/css/tickets.css",
-    "./dist/app.bundle.js",
-    "./dist/events.bundle.js",
-    "./dist/tickets.bundle.js",
-    "./dist/schedule.bundle.js"
-];
+// const APP_PREFIX = 'FoodFest-';
+// const VERSION = 'version_02';
+// const CACHE_NAME = APP_PREFIX + VERSION;
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
-})
+// const FILES_TO_CACHE = [
+//     "./index.html",
+//     "./events.html",
+//     "./tickets.html",
+//     "./schedule.html",
+//     "./assets/css/style.css",
+//     "./assets/css/bootstrap.css",
+//     "./assets/css/tickets.css",
+//     "./dist/app.bundle.js",
+//     "./dist/events.bundle.js",
+//     "./dist/tickets.bundle.js",
+//     "./dist/schedule.bundle.js"
+// ];
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
+// self.addEventListener('install', function (e) {
+//     e.waitUntil(
+//         caches.open(CACHE_NAME).then(function (cache) {
+//             console.log('installing cache : ' + CACHE_NAME)
+//             return cache.addAll(FILES_TO_CACHE)
+//         })
+//     )
+// })
 
-            return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
-        })
-    );
-});
+// self.addEventListener('activate', function (e) {
+//     e.waitUntil(
+//         caches.keys().then(function (keyList) {
+//             let cacheKeeplist = keyList.filter(function (key) {
+//                 return key.indexOf(APP_PREFIX);
+//             });
+//             cacheKeeplist.push(CACHE_NAME);
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
+//             return Promise.all(
+//                 keyList.map(function (key, i) {
+//                     if (cacheKeeplist.indexOf(key) === -1) {
+//                         console.log('deleting cache : ' + keyList[i]);
+//                         return caches.delete(keyList[i]);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+// });
 
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
-        })
-    )
-})
+// self.addEventListener('fetch', function (e) {
+//     console.log('fetch request : ' + e.request.url)
+//     e.respondWith(
+//         caches.match(e.request).then(function (request) {
+//             if (request) { // if cache is available, respond with cache
+//                 console.log('responding with cache : ' + e.request.url)
+//                 return request
+//             } else {       // if there are no cache, try fetching request
+//                 console.log('file is not cached, fetching : ' + e.request.url)
+//                 return fetch(e.request)
+//             }
+
+//             // You can omit if/else for console.log & put one line below like this too.
+//             // return request || fetch(e.request)
+//         })
+//     )
+// })
